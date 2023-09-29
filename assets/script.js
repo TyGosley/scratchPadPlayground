1829295ebf5c432ea898429679918cd0a59af040
TyGosley <tygosley@gmail.com>
2023-09-29 12:56:46 -0700

    Changed hamburger to magic light indicator.  Need to clean up code

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,35 @@
 // Hamburger Menu JavaScript
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    });
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+//     });
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    }));
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+//     }));
+
+const links = document.querySelectorAll('.nav__link');
+const light = document.querySelector('.nav__light');
+
+function moveLight({offsetLeft, offsetWidth}){
+  light.style.left = `${offsetLeft - offsetWidth/4}px`;
+}
+
+function activeLink(linkActive){
+  links.forEach(link => {
+    link.classList.remove('active');
+    linkActive.classList.add('active');
+  })
+}
+
+
+links.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    moveLight(event.target);
+    activeLink(link);
+  })
+})
